2fe9b9d4048bdd527d5c074880f527488195a66e
Thyago Monnerat <101063531+Thyago-Monnerat@users.noreply.github.com>
2025-08-22 20:48:19 +0000

    Refatoração do código nível novato Exclusão do código nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,131 +6,40 @@
 
 #define LINHAS 10
 #define COLUNAS 10
+#define TAMANHONAVIO 3
 
 int checarSeCabeNoTabuleiro(int i);
+void exibirTabuleiro(int matriz[LINHAS][COLUNAS]);
+void colocarNavioHorizontal(int navioHorizontal[2], int tabuleiro[LINHAS][COLUNAS]);
+void colocarNavioVertical(int navioVertical[2], int tabuleiro[LINHAS][COLUNAS]);
 
-int main() {
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    printf("\n====================== TABULEIRO NÍVEL NOVATO ======================\n\n");
-    const char colunas[COLUNAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'} ;
-    int tabuleiro[10][10];
-    int navioX[2][3] = {{3}, {4,5,6}};
-    int navioY[2][3] = {{7,8,9}, {2}};
+    int tabuleiro[LINHAS][COLUNAS];
+    int navioHorizontal[2] = {1, 6};
+    int navioVertical[2] = {2, 4};
 
-    printf("   ");
-
-    //printando as letras das colunas
-    for(int i = 0; i < COLUNAS; i++){
-        printf("%c ", colunas[i]);
+    for (int i = 0; i < LINHAS; i++)
+    {
+        for (int j = 0; j < COLUNAS; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
     }
 
-    printf("\n");
-
-    //condição para adicionar um navio horizontalmente e verticalmente
-    for(int i = 0; i < 3; i++){
-        if(checarSeCabeNoTabuleiro(navioX[1][i]) == 1 || checarSeCabeNoTabuleiro(navioX[0][0]) == 1){
-            printf("Reta viola os limites");
-            return 0;
-        }
-         if(checarSeCabeNoTabuleiro(navioY[0][i]) == 1 || checarSeCabeNoTabuleiro(navioY[1][0]) == 1){
-            printf("Reta viola os limites");
-            return 0;
-        }
-        tabuleiro[navioX[0][0]][navioX[1][i]] = 3;
-        tabuleiro[navioY[0][i]][navioY[1][0]] = 3;
-    }
-
-    for(int i = 0; i < LINHAS; i++){
-        //condicional apenas para adicionar, ou não, um espaço no começo das linhas, para alinhar as linhas 1 a 9 com a linha 10
-        if(i < 9){
-            printf(" %d ", i+1);
-        }else{
-            printf("%d ", i+1);
-        }
-
-        for(int j=0; j < COLUNAS; j++){
-            //condição para popular o tabuleiro com 0's sem alterar os navios
-            if(tabuleiro[i][j] != 3){
-                tabuleiro[i][j] = 0;
-            }
-                
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+    colocarNavioHorizontal(navioHorizontal, tabuleiro);
+    colocarNavioVertical(navioVertical, tabuleiro);
+    exibirTabuleiro(tabuleiro);
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    printf("\n====================== TABULEIRO NÍVEL AVENTUREIRO ======================\n\n");
-    //coordenadas para navio em diagonal decrescente
-    int navioDiagonal1[3] = {3,7};
-    //coordenadas para navio em diagonal crescente
-    int navioDiagonal2[3] = {0,3};
-
-    printf("   ");
-
-    //printando as letras das colunas
-    for(int i = 0; i < COLUNAS; i++){
-        printf("%c ", colunas[i]);
-    }
-
-    printf("\n");
-
-    for(int i = 0; i < 3; i++){
-        if(checarSeCabeNoTabuleiro(navioDiagonal1[i]) == 1){
-            printf("Reta viola os limites");
-            return 0;
-        }
-        if(checarSeCabeNoTabuleiro(navioDiagonal2[i]) == 1){
-            printf("Reta viola os limites");
-            return 0;
-        }
-    }
-
-    //condição para adicionar um navio na diagonal decrescente
-    for(int i = 0; i < LINHAS; i++){
-        if(i > navioDiagonal1[0] && i < navioDiagonal1[1]){
-            if(tabuleiro[i][i] == 3){
-                printf("Sobreposição ou coordenada fora dos limites do tabuleiro");
-                return 0;
-            }   
-            tabuleiro[i][i] = 3;
-        }
-
-        if(i >= navioDiagonal2[0] && i < navioDiagonal2[1]){
-            if(tabuleiro[i][9-i] == 3){
-                printf("Sobreposição ou coordenada fora dos limites do tabuleiro");
-                return 0;
-            }
-            tabuleiro[i][9 - i] = 3;
-        }
-    }
-
-    for(int i = 0; i < LINHAS; i++){
-        //condicional apenas para adicionar, ou não, um espaço no começo das linhas, para alinhar as linhas 1 a 9 com a linha 10
-        if(i < 9){
-            printf(" %d ", i+1);
-        }else{
-            printf("%d ", i+1);
-        }
-
-        for(int j=0; j < COLUNAS; j++){
-            //condição para popular o tabuleiro com 0's sem alterar os navios
-            if(tabuleiro[i][j] != 3){
-                tabuleiro[i][j] = 0;
-            }
-                
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
@@ -142,7 +51,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
@@ -156,8 +65,86 @@ int main() {
     return 0;
 }
 
-int checarSeCabeNoTabuleiro(int i){
-    if(i < 0 || i > 9){
-        return 1;
+void colocarNavioHorizontal(int navioHorizontal[2], int tabuleiro[10][10]){
+    for (int i = 0; i < TAMANHONAVIO; i++)
+    {
+        if (!checarSeCabeNoTabuleiro(navioHorizontal[0] + i) || !checarSeCabeNoTabuleiro(navioHorizontal[1] + i))
+        {
+            printf("Navio horizontal viola os limites do tabuleiro");
+            return;
+        }
+        else if(tabuleiro[navioHorizontal[0]][navioHorizontal[1] + i] == 3)
+        {
+            printf("Erro ao colocar navio horizontal: Está sobrepondo outro navio!");
+            return;
+        }
+        else{
+              tabuleiro[navioHorizontal[0]][navioHorizontal[1] + i] = 3;
+        }
     }
+}
+
+void colocarNavioVertical(int navioVertical[2], int tabuleiro[10][10]){
+     for (int i = 0; i < TAMANHONAVIO; i++)
+     {
+         if (!checarSeCabeNoTabuleiro(navioVertical[0] + i) || !checarSeCabeNoTabuleiro(navioVertical[1] + i))
+        {
+            printf("Navio vertical viola os limites do tabuleiro");
+            return;
+        }
+        else if(tabuleiro[navioVertical[0]+i][navioVertical[1]] == 3)
+        {
+            printf("Erro ao colocar navio vertical: Está sobrepondo outro navio!");
+            return;
+        }
+        else{
+            tabuleiro[navioVertical[0]+i][navioVertical[1]] = 3;
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[10][10])
+{
+    printf("\n====================== TABULEIRO NÍVEL NOVATO ======================\n\n");
+
+    const char colunas[COLUNAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    // Espaço antes de começar as colunas para alinhar com o tabuleiro no terminal
+    printf("   ");
+
+    // Exibição das letras representando as colunas
+    for(int i = 0; i < COLUNAS; i++){
+        printf("%c ", colunas[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < LINHAS; i++)
+    {
+        // Condicional apenas para adicionar, ou não, um espaço no começo das linhas, para alinhar as linhas 1 a 9 com a linha 10
+        if (i < 9)
+        {
+            printf(" %d ", i + 1);
+        }
+        else
+        {
+            printf("%d ", i + 1);
+        }
+
+        for (int j = 0; j < COLUNAS; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int checarSeCabeNoTabuleiro(int i)
+{
+    if (i < 0 || i > 9)
+    {
+        return 0;
+    }
+
+    return 1;
 }
